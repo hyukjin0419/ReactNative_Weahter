3f4f2977d0d99fa572f513a3087ce792328acbc1
hyukjin0419 <21900771@handong.ac.kr>
2023-12-26 13:38:48 +0900

    style study

--- App.js
+++ App.js
@@ -3,16 +3,16 @@ import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={{ flex: 1 }}>
-      <View
-        style={{ flex: 1, backgroundColor: "tomato", flexDirection: "row" }}
-      >
-        <View style={{ flex: 1, backgroundColor: "green" }}></View>
-        <View style={{ flex: 1, backgroundColor: "purple" }}></View>
-        <View style={{ flex: 1, backgroundColor: "yellow" }}></View>
+    <View style={styles.container}>
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Seoul</Text>
       </View>
-      <View style={{ flex: 1, backgroundColor: "blue" }}></View>
-      <View style={{ flex: 1, backgroundColor: "white" }}></View>
+      <View style={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </View>
     </View>
   );
 }
@@ -20,8 +20,30 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
+    backgroundColor: "tomato",
+  },
+  city: {
+    flex: 1.2,
     justifyContent: "center",
+    alignItems: "center",
+  },
+  cityName: {
+    fontSize: 68,
+    fontWeight: "500",
+  },
+  weather: {
+    flex: 3,
+  },
+  day: {
+    flex: 1,
+    alignItems: "center",
+  },
+  temp: {
+    marginTop: 50,
+    fontSize: 178,
+  },
+  description: {
+    marginTop: -30,
+    fontSize: 60,
   },
 });
