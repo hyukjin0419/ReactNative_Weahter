17fe48fdc6564e4d4e0da496d1fcd58b30773d3f
hyukjin0419 <21900771@handong.ac.kr>
2023-12-26 12:26:25 +0900

    starting the project. Practice with layout

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1 }}>
+      <View
+        style={{ flex: 1, backgroundColor: "tomato", flexDirection: "row" }}
+      >
+        <View style={{ flex: 1, backgroundColor: "green" }}></View>
+        <View style={{ flex: 1, backgroundColor: "purple" }}></View>
+        <View style={{ flex: 1, backgroundColor: "yellow" }}></View>
+      </View>
+      <View style={{ flex: 1, backgroundColor: "blue" }}></View>
+      <View style={{ flex: 1, backgroundColor: "white" }}></View>
     </View>
   );
 }
@@ -13,8 +20,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
