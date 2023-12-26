9e9637a772e977b64dc2db72e95e2b31d6dc0e30
hyukjin0419 <21900771@handong.ac.kr>
2023-12-26 15:23:54 +0900

    location 추가

--- App.js
+++ App.js
@@ -1,4 +1,12 @@
+/*location component 사용하여 사용자 위치 불러오기
+1. import "expo-location"
+2. 사용자에게 권한 요청하기
+3. 권한이 생기면 현재 위치의 위도 경도 가져오기,
+4. 현재 위치의 위도 경도를가지고 reverseGeo..함수를 사용하여 현재 도시를 가져와서 변수에 저장하기
+*/
+import * as Location from "expo-location";
 import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useState } from "react";
 import {
   StyleSheet,
   Text,
@@ -11,10 +19,33 @@ import {
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
 export default function App() {
+  //React JS 코드로 진행
+
+  const [city, setCity] = useState("Loading...");
+  const [location, setLocation] = useState();
+  const [ok, setOk] = useState(true);
+  const ask = async () => {
+    const { granted } = await Location.requestForegroundPermissionsAsync();
+    console.log(granted);
+    if (!granted) {
+      setOk(false);
+    }
+    const {
+      coords: { latitude, longitude },
+    } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+    const location = await Location.reverseGeocodeAsync(
+      { latitude, longitude },
+      { useGoogleMaps: false }
+    );
+    setCity(location[0].city);
+  };
+  useEffect(() => {
+    ask();
+  }, []);
   return (
     <View style={styles.container}>
       <View style={styles.city}>
-        <Text style={styles.cityName}>Seoul</Text>
+        <Text style={styles.cityName}>{city}</Text>
       </View>
       <ScrollView
         pagingEnabled
@@ -54,7 +85,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "yellow",
+    backgroundColor: "teal",
   },
   city: {
     flex: 1.2,
